8bff2f1b5f53259d1591aab86e2dbdecaabf1b10
SupitsRajeev <146926341+SupitsRajeev@users.noreply.github.com>
2026-02-21 22:32:49 +0545

    Added new section and managed the case sensitive imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Navbar from "./components/Navbar";
-import About from "./components/about";
-import Projects from "./components/projects";
-import Contact from "./components/contact";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Blog from "./components/Blog";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <About />
       <Projects />
+      <Blog />
       <Contact />
     </div>
   );
---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>MySite</h2>
+      <ul>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
