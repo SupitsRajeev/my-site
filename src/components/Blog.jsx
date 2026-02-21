ff05d69f1aac316b579832a69c7f961baba9f08b
SupitsRajeev <146926341+SupitsRajeev@users.noreply.github.com>
2026-02-21 22:44:20 +0545

    Fixed error

---
+++ src/components/Blog.jsx
@@ -0,0 +1,25 @@
+const posts = [
+  {
+    id: 1,
+    title: "My Coding Journey",
+    date: "Feb 2026",
+    content: "I started learning React and building my own website..."
+  },
+  {
+    id: 2,
+    title: "Why I Love Programming",
+    date: "Feb 2026",
+    content: "Programming allows me to build ideas into reality..."
+  }
+];
+
+function Blog() {
+  return (
+    <section>
+      <h2>My Blog</h2>
+      <p>This is my blog section.</p>
+    </section>
+  );
+}
+
+export default Blog;
